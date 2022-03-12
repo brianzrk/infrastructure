05e8b98d172a8bdee32eddeacb5d5cbb03191ce4
brianzrk <brianzhangruikai@163.com>
2022-03-13 01:47:19 +0800

    [story]添加打印beanName的逻辑， 使用CommandLine

--- src/main/java/com/origin/infrastructure/InfrastructureApplication.java
+++ src/main/java/com/origin/infrastructure/InfrastructureApplication.java
@@ -1,13 +1,33 @@
 package com.origin.infrastructure;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class InfrastructureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InfrastructureApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            // asc升序
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 
 }
